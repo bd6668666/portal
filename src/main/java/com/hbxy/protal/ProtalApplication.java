d480313ed761b0c7e6a2d04b6d7e5ae1b085bc7d
xbd <851188351@qq.com>
2019-05-15 10:43:21 +0800

    项目结构搭建

---
+++ src/main/java/com/hbxy/protal/ProtalApplication.java
@@ -0,0 +1,13 @@
+package com.hbxy.protal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtalApplication.class, args);
+    }
+
+}
